8c87d73bf276e2b96735ccc9c19d8d2a97f1823c
João Vitor Santiago de Oliveira Braz <jvsobraz@hotmail.com>
2023-04-26 17:43:46 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import { useFonts, Lobster_400Regular } from '@expo-google-fonts/lobster';
 import { ActivityIndicator } from 'react-native';
 
-import Home from "./src/screens/Home";
-import Phrases from "./src/screens/Phrases";
-import { ActivityIndicator } from 'react-native-web';
+import Home from './src/screens/Home';
+import Phrases from './src/screens/Phrases';
 
 const Tab = createBottomTabNavigator();
 
@@ -16,10 +15,10 @@ export default function App() {
       <Tab.Navigator
         screenOptions={{
           headerShown: false,
-          tabBarActiveTintColor: "#ffffff",
+          tabBarActiveTintColor: '#ffffff',
           tabBarStyle: {
-            backgroundColor: "#000000",
-            borderTopColor: "#000000",
+            backgroundColor: '#000000',
+            borderTopColor: '#000000',
           }
         }}
         initialRouteName='Home'
@@ -28,20 +27,19 @@ export default function App() {
           name="Home"
           component={Home}
           options={{
-            tabBarLabel: "Frases",
+            tabBarLabel: 'Frases',
             tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="arm-flex" color={color} size={30} />
+              <MaterialCommunityIcons name='arm-flex' color={color} size={30} />
             )
           }}
         />
-
         <Tab.Screen
           name="Phrases"
           component={Phrases}
           options={{
-            tabBarLabel: "Todas as frases",
+            tabBarLabel: 'Todas as frases',
             tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="format-list-bulleted" color={color} size={30} />
+              <MaterialCommunityIcons name='format-list-bulleted' color={color} size={30} />
             )
           }}
         />
@@ -51,7 +49,7 @@ export default function App() {
 
   let [fontsLoaded] = useFonts({ Lobster_400Regular });
 
-  if(!fontsLoaded) {
+  if (!fontsLoaded) {
     return <ActivityIndicator />
   }
 
